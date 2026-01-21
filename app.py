f045b8f63207ca9207e3d8589cfc562e6154c609
Arsh <q43676709@gmail.com>
2026-01-21 12:35:22 +0530

    requirements.txt and text based structure for app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 #we'll set up API Routes first.
+#Let's import the required libraries first
+
+
+
+#Set up and Flask Configuration
+
+
+#Gemini Client set up
+
+
+#Data Models( Ye Pydantic ka kaam h for structured outputs, we'll design classes)
+
+#REST Routes 
+
+
+
+
+#WEBSOCKET EVENTS 
