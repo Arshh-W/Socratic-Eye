f4aca1b4637bc0a0efb1cec9aa1965213bccbca4
Arsh <q43676709@gmail.com>
2026-01-21 14:14:18 +0530

    REST Routes and function structures

--- app.py
+++ app.py
@@ -25,7 +25,29 @@ client= genai.Client(api_key=os.enviorn.get("GEMINI_API_KEY"),
 
 As for the GET Route, We'll send a report about the entire session to the user so that they know 
 what all they learned, and save it in the database whenever the user hits end session."""
+sessions={} #temporary storage baadme isse database se connect krdenge
+@app.route('/auth/session',methods=['POST'])
+def start_session():
+    """It will start our AI, uska function mai baadme daaldunga, basic structure is gonna be this, and probably 
+    we'll also add login/singups so wo bhi handle krlenge isme hi"""
+    session_id= request.json.get('session_id','anonymous')
+    sessions[session_id]= #I'll make a class function which will store the details in the backend later.
+    return jsonify({
+        "status":"Online",
+        "session_id":session_id
+    })
+@app.route('/report',methods=['GET'])
+def generate_report():
+    """It will generate a session report based of the session memory from the current session
+    and then later(I'll connect it to the database for storing the sessions for users to look back at)
+    """
+    report_string= report_generation()#We'll create this function in another py script, and will import it later
+    #later, I'll manage the database connectivty
+    return jsonify({
+        "report": report_string,
+        "message": " We're signing off, Hope to see you soon"
 
+    })
 
 #WEBSOCKET EVENTS 
 
